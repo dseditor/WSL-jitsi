39a8864f30800b731ee1bab68b5cd1939b871822
dseditor <91339497+dseditor@users.noreply.github.com>
2021-09-25 00:51:51 +0800

    移除程式及啟動服務腳本

---
+++ remove-jitsi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt purge jigasi jitsi-meet jitsi-meet-web-config jitsi-meet-prosody jitsi-meet-turnserver jitsi-meet-web jicofo jitsi-videobridge2
+sudo apt autoremove
---
+++ wsl-service.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo /etc/init.d/nginx start 
+sudo /etc/init.d/jicofo start 
+sudo /etc/init.d/jitsi-videobridge2 start
+sudo /etc/init.d/prosody start 
